5343b81834c0a3c9a861b8682ed4a1821ae53ba9
jerry1177 <jerrymaldonadom@gmail.com>
2021-01-16 08:55:47 -0800

    changed nothing

--- WebService1/Models/TestModel.cs
+++ WebService1/Models/TestModel.cs
@@ -8,10 +8,12 @@ namespace WebService1.Models
 {
     public class TestModel
     {
+        
         [Required] // make sure this field is not null attribute
         [StringLength(20)]
         public string TestName { get; set; }
 
+       
         [Required]
         [StringLength(10)] // make sure the string is no longer than 10 characters attribute
         public string TestStreet { get; set; }
